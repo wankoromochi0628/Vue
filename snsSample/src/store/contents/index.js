024e3e982250f9b29fa1751f7d948f486b4625bf
鈴木優人 <wankoromoti0628@gmail.com>
2019-03-05 22:12:46 +0900

    vuexファイル追加

---
+++ snsSample/src/store/contents/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const state = {
+    count: 0
+};
+
+const mutations = {
+    countUp(state,payload) {
+        state.count += 1;
+    }
+};
+
+const actions = {
+    addNumber({commit},payload) {
+        commit('countUp',payload);
+    }
+};
+
+export default new Vuex.Store({
+    state: state,
+    mutations: mutations,
+    actions: actions
+});
